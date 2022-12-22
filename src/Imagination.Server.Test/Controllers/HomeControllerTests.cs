7929b494e8792dafba4282911f774bb8d89450bb
Hedayat <hedayat@coffeechatnetwork.io>
2022-12-22 21:41:24 +0330

    Home controller and tests

---
+++ src/Imagination.Server.Test/Controllers/HomeControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Imagination.Controllers;
+using Imagination.Server.App.Services.Interfaces;
+using Imagination.Server.App.Types;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Imagination.Server.Test.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Theory]
+        [InlineData(ServiceErrorType.EmptyOrNullStream)]
+        [InlineData(ServiceErrorType.EmptyImageOrUnknownFormat)]
+        [InlineData(ServiceErrorType.Unknown)]
+        public async Task Convert_Should_Return_UnSupportable_Enitity_When_ServiceResult_Has_Error(ServiceErrorType error)
+        {
+
+            var serviceResult = new ServiceResult(error);
+            var imaginationServiceMock = new Mock<IImageConvertorService>();
+            imaginationServiceMock.Setup(x => x.ConvertToJpeg(It.IsAny<MemoryStream>())).Returns(serviceResult);
+
+            var homeController = CreateHomeControllerWithHttpContext(new MemoryStream(), imaginationServiceMock);
+
+            var result = await homeController.Convert();
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<UnprocessableEntityObjectResult>();
+
+        }
+
+        [Fact]
+        public async Task Convert_Should_Return_File_When_ServiceResult_Is_Successfull()
+        {
+
+            var serviceResult = new ServiceResult(new MemoryStream());
+            var imaginationServiceMock = new Mock<IImageConvertorService>();
+            imaginationServiceMock.Setup(x => x.ConvertToJpeg(It.IsAny<MemoryStream>())).Returns(serviceResult);
+
+            var homeController = CreateHomeControllerWithHttpContext(new MemoryStream(), imaginationServiceMock);
+
+            var result = await homeController.Convert();
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<FileContentResult>();
+
+        }
+
+
+        public HomeController CreateHomeControllerWithHttpContext(Stream stream, Mock<IImageConvertorService> mockedService)
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Body = stream;
+            httpContext.Request.ContentLength = 0;
+
+            return new HomeController(mockedService.Object)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = httpContext
+                }
+            };
+        }
+    }
+}
--- src/Imagination.Server/Controllers/HomeController.cs
+++ src/Imagination.Server/Controllers/HomeController.cs
@@ -1,9 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Imagination.Server.App.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Imagination.Controllers
 {
     [ApiController]
     public class HomeController : ControllerBase
     {
+        private readonly IImageConvertorService _imageConvertorService;
+
+        public HomeController(IImageConvertorService imageConvertorService)
+        {
+            _imageConvertorService = imageConvertorService;
+        }
+
+        [Route("convert")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public async Task<IActionResult> Convert()
+        {
+            var imageStream = new MemoryStream();
+            await Request.Body.CopyToAsync(imageStream);
+            var result = _imageConvertorService.ConvertToJpeg(imageStream);
+
+
+            if (result.WasSuccessful)
+            {
+                return File(result.Result.ToArray(), "image/jpg");
+            }
+
+            return UnprocessableEntity(result.Error.ToString());
+
+        }
     }
 }
